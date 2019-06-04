5a6e101d4c1597dc26f81072717ddda2cd0ec99d
Lucas Wonderley <lucas.r.wonderley@gmail.com>
2019-06-04 09:46:08 -0400

    Use dimensions to start to lay out tiles

--- react-app/src/components/GridComponent.tsx
+++ react-app/src/components/GridComponent.tsx
@@ -10,21 +10,59 @@ export interface GridProps {
   name: string;
 }
 
+export interface GridState {
+  dimensions: {
+    height: number,
+    width: number,
+  } | null;
+}
+
 export class GridComponent
-       extends React.Component<GridProps>
+       extends React.Component<GridProps, GridState>
        implements GridView {
-  private _currentSubrow: number = 0;
-  private _rows: any[][] = [];
+  private _container: HTMLDivElement | null = null;
+
+  constructor(props: any, context?: any) {
+    super(props, context);
+    this.state = { dimensions: null }
+  }
+
   render() {
+    const { dimensions } = this.state;
     return (
-      <div className="grid">
-        <TileComponent/>
+      <div className="grid" ref={el => (this._container = el)}>
+        {dimensions && this.renderContent(dimensions)}
       </div>
     );
   }
+
+  renderContent(dimensions: { height: number, width: number }): JSX.Element[] {
+    const tileHeight = dimensions.height / Grid.ROW_COUNT;
+    const tileWidth = dimensions.width / Grid.COLUMN_COUNT;
+    const tiles: JSX.Element[] = [];
+    for (let y = 0; y < Grid.ROW_COUNT; ++y) {
+      for (let x = 0; x < Grid.COLUMN_COUNT; ++x) {
+        const top = tileHeight * y;
+        const left = tileWidth * x;
+        tiles.push(<TileComponent
+                      tile={this.props.grid.tileAt(x,y)}
+                      top={top}
+                      left={left} />);
+      }
+    }
+    return tiles;
+  }
+
+  componentDidMount() {
+    this.setState({
+      dimensions: {
+        width: this._container!.offsetWidth,
+        height: this._container!.offsetHeight,
+      },
+    });
+  }
+
   initializeView(): void {
-    const rowCount = Grid.ROW_COUNT;
-    const colCount = Grid.COLUMN_COUNT;
     // create a tile holder view
     // create the tiles and place them in it
     // add a cursor
@@ -37,19 +75,19 @@ export class GridComponent
     // update the tile locations
     // update tile holder view location
 
-    const subrowAdjustment = this._currentSubrow - this.props.grid.currentSubrow;
-    this._currentSubrow = this.props.grid.currentSubrow;
-    for (let y = 0; y < Grid.ROW_COUNT; ++y) {
-      let isBottomRow = (y === Grid.ROW_COUNT - 1);
-      for (let x = 0; x < Grid.COLUMN_COUNT; ++x) {
-        let tile = this.props.grid.tileAt(x,y);
-        let tileView = this._rows[y][x];
+    // const subrowAdjustment = this._currentSubrow - this.props.grid.currentSubrow;
+    // this._currentSubrow = this.props.grid.currentSubrow;
+    // for (let y = 0; y < Grid.ROW_COUNT; ++y) {
+    //   let isBottomRow = (y === Grid.ROW_COUNT - 1);
+    //   for (let x = 0; x < Grid.COLUMN_COUNT; ++x) {
+    //     let tile = this.props.grid.tileAt(x,y);
+    //     let tileView = this._rows[y][x];
         // use css classes for this
         // let color = this.colorForTile(tile, isBottomRow);
         // tileView.style.bg = color;
         // tileView.position.top += subrowAdjustment;
-      }
-    }
+    //   }
+    // }
     // update cursor
     // this._cursorBox.position.top += subrowAdjustment;
     // render the screen
