4f71049efbb6f5c488e95f0d9111d7bcc6a344db
Lucas Wonderley <lucas.r.wonderley@gmail.com>
2019-06-12 17:54:09 -0400

    Move cursor

--- react-app/src/terminal/input_controller.ts
+++ react-app/src/terminal/input_controller.ts
@@ -5,38 +5,37 @@ import { InputDelegate } from './input_delegate';
 export class InputController implements InputDelegate {
   constructor(private _cursor: Cursor) {
     document.addEventListener("keydown", this.onKeyDown.bind(this));
-    document.addEventListener("keyup", this.onKeyUp.bind(this));
   }
 
-  onKeyDown(e: Event) {
+  onKeyDown(e: KeyboardEvent) {
     debugger;
-  }
-  onKeyUp(e: Event) {
-    debugger;
+    this.onUserInput(e.key);
   }
   onUserInput(key: string) {
     // Quit on Escape, q, or Control-C.
     if (key === 'escape' || key === 'q' || key === 'C-c') {
       return process.exit(0);
     }
-    if (key === 'up' || key === 'k') {
+    if (key === 'ArrowUp' || key === 'k') {
       this._cursor.goUp();
     }
-    if (key === 'down' || key === 'j') {
+    if (key === 'ArrowDown' || key === 'j') {
       this._cursor.goDown();
     }
-    if (key === 'left' || key === 'h') {
+    if (key === 'ArrowLeft' || key === 'h') {
       this._cursor.goLeft();
     }
-    if (key === 'right' || key === 'l') {
+    if (key === 'ArrowRight' || key === 'l') {
       this._cursor.goRight();
     }
-    if (key === 'space') {
+    if (key === ' ') {
       this._cursor.swapTiles();
     }
     if (key === 'a') {
       // Quickly advance the game four times.
-	    let advanceGameWithBoundThis = GameController.instance.advanceGame.bind(GameController);
+      let advanceGameWithBoundThis = 
+        GameController.instance.advanceGame
+        .bind(GameController.instance);
       setTimeout(advanceGameWithBoundThis, 100);
       setTimeout(advanceGameWithBoundThis, 200);
       setTimeout(advanceGameWithBoundThis, 300);
